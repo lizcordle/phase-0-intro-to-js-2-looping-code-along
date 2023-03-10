7ceb56606bb5f108f7a92f5179b3b7783e5f4cb8
Elizabeth Cordle <cordle36@gmail.com>
2023-03-10 09:28:19 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const messageNames = [];
+
+function writeCards(name, value) {
+    let messageNames = []
+    for (let i = 0; i < name.length; i++) {
+        messageNames.push(`Thank you, ${name[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames;
+}
+function countDown() {
+    let numCountdown=11;
+    while(numCountdown--) {
+        console.log(numCountdown)
+    }
+}
